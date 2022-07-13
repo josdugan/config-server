fb83ebc21d5454ed49f6640e1f6101b68883f1d4
josdugan <josdugan@gmail.com>
2022-07-13 13:11:17 -0400

    Initial commit

---
+++ src/test/java/com/josdugan/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josdugan.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
